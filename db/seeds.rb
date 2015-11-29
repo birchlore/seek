16329cc16c67d3116e83c90162c5cd4c61d02476
birchlore <jacksondcunningham@gmail.com>
2015-11-28 17:39:31 -0800

    add homepage description

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+
+
 Movie.create(
               title:'The Hunger Games Part 2',
               description: 'Realizing the stakes are no longer just for survival, Katniss Everdeen (Jennifer Lawrence) teams up with her closest friends, including Peeta (Josh Hutcherson), Gale (Liam Hemsworth) and Finnick for the ultimate mission.',
               embed: '<iframe width="100%" src="https://www.youtube.com/embed/n-7K_OjsDCQ" frameborder="0" allowfullscreen></iframe>'
+            )
+
+Movie.create(
+              title:'Creed',
+              description: 'Adonis Johnson (Michael B. Jordan) never knew his famous father, boxing champion Apollo Creed, who died before Adonis was born. However, boxing is in his blood, so he seeks out Rocky Balboa (Sylvester Stallone) and asks the retired champ to be his trainer.',
+              embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Uv554B7YHk4" frameborder="0" allowfullscreen></iframe>'
             )
 
 Movie.create(
@@ -25,47 +33,6 @@ Movie.create(
               embed: '<iframe width="100%" src="https://www.youtube.com/embed/ShIAH_G-Iuw" frameborder="0" allowfullscreen></iframe>'
             )
 
-Movie.create(
-              title:'Love the Coopers',
-              description: 'For their annual Christmas Eve celebration, Sam Cooper (John Goodman) and his wife Charlotte (Diane Keaton) welcome four generations of extended family, including son Hank (Ed Helms), daughter Eleanor (Olivia Wilde) and Charlottes father, Bucky (Alan Arkin).',
-              embed: '<iframe width="100%" src="https://www.youtube.com/embed/-TSX_0rwPNc" frameborder="0" allowfullscreen></iframe>'
-            )
-
-Movie.create(
-              title:'The Peanuts Movie',
-              description: 'Life always seems complicated for good old Charlie Brown (Noah Schnapp), the boy who always tries his best against seemingly impossible odds. When the Little Red-Haired Girl moves into his neighborhood, Charlie Brown develops a crush on her.',
-              embed: '<iframe width="100%" src="https://www.youtube.com/embed/8pCoVQDD-OY" frameborder="0" allowfullscreen></iframe>'
-            )
-
-Movie.create(
-              title:'Secret in Their Eyes',
-              description: 'Rising FBI investigators Ray (Chiwetel Ejiofor) and Jess (Julia Roberts), along with Claire (Nicole Kidman), their district-attorney supervisor, are suddenly torn apart following the brutal murder of Jess teenage daughter.',
-              embed: '<iframe width="100%" src="https://www.youtube.com/embed/N3b0PFCrayE" frameborder="0" allowfullscreen></iframe>'
-            )
-
-Movie.create(
-              title:'Victor Frankenstein',
-              description: 'While searching for animal body parts at a London circus, radical scientist Victor Frankenstein (James McAvoy) meets gifted surgeon Igor Strausman (Daniel Radcliffe).',
-              embed: '<iframe width="100%" src="https://www.youtube.com/embed/7pxZxY_Siyc" frameborder="0" allowfullscreen></iframe>'
-            )
-
-Movie.create(
-              title:'The Good Dinosaur',
-              description: 'Luckily for young Arlo, his parents (Jeffrey Wright, Frances McDormand) and his two siblings, the mighty dinosaurs were not wiped out 65 million years ago. When a rainstorm washes poor Arlo (Raymond Ochoa) downriver, he ends up bruised, battered and miles away from home. ',
-              embed: '<iframe width="100%" src="https://www.youtube.com/embed/daFnEiLEx70" frameborder="0" allowfullscreen></iframe>'
-            )
-
-Movie.create(
-              title:'The 33',
-              description: 'Disaster strikes on Aug. 5, 2010, as a copper and gold mine collapses in Chile, trapping 33 men underground. With more than 2,000 feet of rock in their way, members of a rescue team work tirelessly for 69 days to save the seemingly doomed crew.',
-              embed: '<iframe width="100%" src="https://www.youtube.com/embed/hOoIBOYqHyw" frameborder="0" allowfullscreen></iframe>'
-            )
-
-Movie.create(
-              title:'Spotlight',
-              description: 'In 2001, editor Marty Baron of The Boston Globe assigns a team of journalists to investigate allegations against John Geoghan, an unfrocked priest accused of molesting more than 80 boys.',
-              embed: '<iframe width="100%" src="https://www.youtube.com/embed/EwdCIpbTN5g" frameborder="0" allowfullscreen></iframe>'
-            )
 
 Movie.create(
               title:'The Martian',
